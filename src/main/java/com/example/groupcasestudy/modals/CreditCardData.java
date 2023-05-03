05100051e806a6559f1add9a9303854523a6f5fa
Dhruv Maheshwari <mrdm026@gmail.com>
2023-05-03 10:25:59 +0530

    Added csv modal data

---
+++ src/main/java/com/example/groupcasestudy/modals/CreditCardData.java
@@ -0,0 +1,39 @@
+package com.example.groupcasestudy.modals;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditCardData {
+    @CsvBindByName
+    private UUID transactionId;
+    @CsvBindByName
+    private String cardHolderName;
+    @CsvBindByName
+    private String creditCardNumber;
+    @CsvBindByName
+    private String transactionDate;
+    @CsvBindByName
+    private String merchantName;
+    @CsvBindByName
+    private String merchantCity;
+    @CsvBindByName
+    private String merchantPostalCode;
+    @CsvBindByName
+    private Double amount;
+    @CsvBindByName
+    private String currencyCode;
+    @CsvBindByName
+    private String transactionStatus;
+    @CsvBindByName
+    private String cardType;
+}
