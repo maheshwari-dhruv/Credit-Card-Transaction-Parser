1f3381bd3ecc965ca2b3c2ab6443a02a6aa778bf
Dhruv Maheshwari <mrdm026@gmail.com>
2023-05-29 02:21:53 +0530

    Added credit card data modal and dto classes

---
+++ src/main/java/com/example/groupcasestudy/modals/dto/CreditCardDataDTO.java
@@ -0,0 +1,38 @@
+package com.example.groupcasestudy.modals.dto;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditCardDataDTO {
+    @CsvBindByName
+    private UUID transactionId;
+    @CsvBindByName
+    private String cardHolderName;
+    @CsvBindByName
+    private String creditCardNumber;
+    @CsvBindByName
+    private String transactionDate;
+    @CsvBindByName
+    private String merchantName;
+    @CsvBindByName
+    private String merchantCity;
+    @CsvBindByName
+    private String merchantPostalCode;
+    @CsvBindByName
+    private Double amount;
+    @CsvBindByName
+    private String currencyCode;
+    @CsvBindByName
+    private String transactionStatus;
+    @CsvBindByName
+    private String cardType;
+}
